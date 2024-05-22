d359dd6c237f5897d7494a7ae92f2830916d7ae3
Dimovoz <166947006+Dimovoz@users.noreply.github.com>
2024-05-22 21:33:54 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,7 +4,9 @@ c = int(input('Введите Длину стороны С: '))
 
 if a == b == c:
     print('Равносторонний')
-elif (a != b and b != c) or ( a == b and b != c) or (b == c and c != a):
-    print('Равнобедренный')
-elif a != b != c:
+    
+elif a != b and a != c and b != c:
     print('Разносторонний')
+
+else:
+    print('Равнобедренный')
