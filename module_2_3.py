6e8ed7d185b4f566bb59ac9693fe7e35fc7f41ae
Dimovoz <166947006+Dimovoz@users.noreply.github.com>
2024-06-01 20:23:22 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,22 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+n = 0
+
+while True:
+    n = my_list[0]
+    if n > 0:
+        print(n)
+    n = my_list[1]
+    if n > 0:
+        print(n)
+    n = my_list[2]
+    if n > 0:
+        print(n)
+    n = my_list[3]
+    if n > 0:
+        print(n)
+    break
+
+
+
+
